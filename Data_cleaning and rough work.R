ff3770a77914f3cdfbe7de66d514e13b6b2699b1
AjayKk27 <ak834@bu.edu>
2023-11-02 21:12:43 -0400

    Cleaned property damage and crop damage columns by making them numeric and multiplying them by the appropriate number depending on the suffix(K or M)

--- Data_cleaning and rough work.R
+++ Data_cleaning and rough work.R
@@ -1,19 +1,17 @@
-library(janeaustenr)
 library(tidyverse)
-original_books <- austen_books() |> group_by(books)
 library(readr)
 
-storms<- read_csv("Downloads/StormEvents_locations-ftp_v1.0_d2021_c20231017.csv")
-
-storms |> filter(LOCATION=="HOT COFFEE")
-
-storms<-read_csv("Downloads/StormEvents_details-ftp_v1.0_d2021_c20231017.csv")
-
-hotcoffee<-storms |>  filter(BEGIN_LOCATION=="HOT COFFEE")
+storms<- read_csv("StormEvents_details_2021.csv")
 
 floods<- storms |> filter(str_detect(EVENT_TYPE,"Flood")==TRUE)
 
 fema<- read_csv("DisasterDeclarationsSummaries.csv")
 fema_floods<- fema |> filter(str_detect(incidentType, "Flood")==TRUE) |> 
                   filter(str_detect(incidentBeginDate,"2021")==TRUE)
+
+fema_assistance<- read.csv("FemaWebDisasterSummaries.csv", header = TRUE) 
+#|> 
+ # filter(str_detect(paLoadDate,"2021")==TRUE)
+
+fema_combined<- left_join(x=fema_floods,y=fema_assistance, by="disasterNumber")
 
